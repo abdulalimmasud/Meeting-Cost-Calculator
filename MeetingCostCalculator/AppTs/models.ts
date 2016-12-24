4ed8286dfe8d054324af5a3236c35427cef851f6
Abdul Alim <abdulalimmasud@gmail.com>
2016-12-24 07:13:54 +0600

    Add Model class

---
+++ MeetingCostCalculator/AppTs/models.ts
@@ -0,0 +1,11 @@
+﻿/// <reference path="_all.ts" />
+
+module meetingCostCalculator {
+    'use strict';
+
+    export class settingsModel {
+        constructor(public people: number, public averageSalary: number) {
+
+        }
+    }
+}
